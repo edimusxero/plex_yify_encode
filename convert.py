c822439815933cd14e24a2598b05b0d927eddc04
edimusxero <edimusrex@gmail.com>
2020-11-14 22:36:53 -0500

    More code changes and other tweaks to the database

--- convert.py
+++ convert.py
@@ -67,12 +67,52 @@ def size_conversion(file_size):
     sys.exit()
 
 
+def get_length(filename):
+    """Returns the length of the media file"""
+    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", filename],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        check=True)
+
+    length_in_seconds = float(result.stdout)
+
+    return str(round(length_in_seconds / 60, 2))
+
+
+def test_completion(src_file, dest_file, file_name):
+    """Tests Length Of The Converted File to Ensure Completion"""
+    if src_file == dest_file:
+        try:
+            sql_query = "UPDATE compressed_files\
+                        SET Response = (SELECT Id\
+                        FROM assoc_process_response WHERE\
+                        Response = 'Yes'),\
+                        Processing = (SELECT Id\
+                        FROM assoc_process_response WHERE\
+                        Response = 'No')\
+                        WHERE NAME = %s"
+
+            CURSOR.execute(sql_query, (file_name))
+            CONN.commit()
+            return True
+
+        except pymysql.OperationalError as op_error:
+            print(op_error)
+            CONN.rollback()
+            return False
+
+    return False
+
+
 def convert_file(root, file):
     """Builds to the command to the FFMPEG File Conversion"""
     src = os.path.join(root, file)
     dest = os.path.join(DESTINATION, file)
 
     try:
+        print("Converting -- " + file)
         subprocess.call(['ffmpeg',
                          '-i',
                          src,
@@ -90,9 +130,15 @@ def convert_file(root, file):
                          'aac',
                          dest
                         ])
+
+        return test_completion(get_length(src), get_length(dest), file)
+
     except RuntimeError as error:
         print(error)
-        sys.exit()
+        sql_query = "DELETE FROM compressed_files WHERE name = %s"
+        CURSOR.execute(sql_query, file)
+        os.remove(dest)
+        return False
 
 
 def process_files_for_compression(file_root, files_for_processing):
@@ -106,41 +152,46 @@ def process_files_for_compression(file_root, files_for_processing):
 
             if result is not None:
                 plex_id = result[0]
-                sql_query = "SELECT Size, (SELECT Response FROM assoc_process_response\
-                             WHERE compressed_files.Response = Id) AS Response\
+                sql_query = "SELECT Size,\
+                             (SELECT Response FROM assoc_process_response\
+                             WHERE compressed_files.Response = Id) AS Response,\
+                             (SELECT Response From assoc_process_response\
+                             WHERE compressed_files.Processing = Id) AS Processing\
                              FROM compressed_files WHERE Id = %s"
+
                 CURSOR.execute(sql_query, plex_id)
                 result = CURSOR.fetchone()
 
                 file_size = result[0]
                 response = result[1]
+                processing = result[2]
 
-                if file_size > size and response not in 'Yes':
+                if file_size > size and response == 'Yes' and processing == 'No':
                     print("Converting to a Smaller file " + name)
                     sql_query = "UPDATE compressed_files SET Size = %s WHERE name = %s"
                     CURSOR.execute(sql_query, (size, name))
                     CONN.commit()
                     continue
 
-                if response in 'No':
-                    print("Previous Run Must Have Failed.  Converting -- " + name)
-                    set_proc_query = "UPDATE compressed_files\
-                                      SET Processing = (SELECT Id FROM\
-                                      assoc_process_response WHERE Response = 'Yes')\
-                                      WHERE NAME = %s"
-                    CURSOR.execute(sql_query, (name))
-                    CONN.commit()
-                    convert_file(file_root, name)
-                    sql_query = "UPDATE compressed_files\
-                                SET Response = (SELECT Id FROM\
-                                assoc_process_response WHERE Response = 'Yes') WHERE NAME = %s"
-                    CURSOR.execute(sql_query, (name))
-                    CONN.commit()
+                if response == 'No' and processing == 'Yes':
+                    print(name + " is currently processing")
+                    continue
 
             else:
+                if os.path.isfile(os.path.join(DESTINATION, name)):
+                    print(name + " : exists and may be processing by a seperate server")
+                    continue
+
                 if size > size_conversion(SIZE):
                     try:
-                        sql_query = "INSERT INTO compressed_files(Name, Size) VALUES (%s, %s)"
+                        sql_query = "INSERT INTO compressed_files\
+                                     (Name, Size, Response, Processing)\
+                                     VALUES (%s, %s,\
+                                     (SELECT Id FROM assoc_process_response WHERE\
+                                     Response = 'No'),\
+                                     (SELECT Id FROM assoc_process_response WHERE\
+                                     Response = 'Yes'))"
+
                         CURSOR.execute(sql_query, (name, size))
                         CONN.commit()
 
@@ -148,23 +199,11 @@ def process_files_for_compression(file_root, files_for_processing):
                         print(op_error)
                         CONN.rollback()
 
-                    print("Converting -- " + name)
-                    set_proc_query = "UPDATE compressed_files\
-                                      SET Processing = (SELECT Id FROM\
-                                      assoc_process_response WHERE Response = 'Yes')\
-                                      WHERE NAME = %s"
-                    CURSOR.execute(set_proc_query, (name))
-                    CONN.commit()
-                    convert_file(file_root, name)
-                    sql_query = "UPDATE compressed_files\
-                                 SET Response = (SELECT Id\
-                                 FROM assoc_process_response WHERE\
-                                 Response = 'Yes'),\
-                                 Processing = (SELECT Id\
-                                 FROM assoc_process_response WHERE\
-                                 Response = 'No') WHERE NAME = %s"
-                    CURSOR.execute(sql_query, (name))
-                    CONN.commit()
+                    if convert_file(file_root, name):
+                        print("File conversion successful")
+
+                    else:
+                        print("File conversion has failed")
 
 
 def main():
--- database.sql
+++ database.sql
@@ -1,19 +1,5 @@
-CREATE DATABASE IF NOT EXISTS plex_files;
-
-USE plex_files;
-
-CREATE TABLE IF NOT EXISTS `compressed_files` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Size` bigint NOT NULL DEFAULT '0',
-  `Response` int NOT NULL DEFAULT '0',
-  `Processing` int DEFAULT '1',
-  PRIMARY KEY (`Id`),
-  KEY `FK_compressed_files_assoc_process_response` (`Response`),
-  KEY `FK_compressed_files_assoc_process_response_2` (`Processing`),
-  CONSTRAINT `FK_compressed_files_assoc_process_response` FOREIGN KEY (`Response`) REFERENCES `assoc_process_response` (`Id`),
-  CONSTRAINT `FK_compressed_files_assoc_process_response_2` FOREIGN KEY (`Processing`) REFERENCES `assoc_process_response` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1444 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS `plex_files`
+USE `plex_files`;
 
 CREATE TABLE IF NOT EXISTS `assoc_process_response` (
   `Id` int NOT NULL DEFAULT '0',
@@ -21,9 +7,16 @@ CREATE TABLE IF NOT EXISTS `assoc_process_response` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DELETE FROM `assoc_process_response`;
-INSERT INTO `assoc_process_response` (`Id`, `Response`) VALUES
-	(0, 'No'),
-	(1, 'Yes');
-
-SHOW WARNINGS LIMIT 0;
+CREATE TABLE IF NOT EXISTS `compressed_files` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Size` bigint NOT NULL DEFAULT '0',
+  `Response` int NOT NULL DEFAULT '0',
+  `Processing` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Name` (`Name`),
+  KEY `FK_compressed_files_assoc_process_response` (`Response`),
+  KEY `FK_compressed_files_assoc_process_response_2` (`Processing`),
+  CONSTRAINT `FK_compressed_files_assoc_process_response` FOREIGN KEY (`Response`) REFERENCES `assoc_process_response` (`Id`),
+  CONSTRAINT `FK_compressed_files_assoc_process_response_2` FOREIGN KEY (`Processing`) REFERENCES `assoc_process_response` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
